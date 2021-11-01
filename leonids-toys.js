0f19d1dbf1a3ce474f16f329aa2be4177cc8a9a2
Jake Chestnut <jacobschestnut@gmail.com>
2021-11-01 18:10:33 -0500

    my first commit

---
+++ leonids-toys.js
@@ -0,0 +1,31 @@
+const toys = [
+    {
+        id: 1,
+        name: "Lightsaber",
+        brand: "Disney",
+        franchise: "Star Wars",
+        age: "4+",
+        manufacturer: "Hasbro",
+        price: 19.99
+    },
+    {
+        id: 2,
+        name: "Hulk Hands",
+        brand: "Marvel",
+        franchise: "The Incredible Hulk",
+        age: "3+",
+        manufacturer: "Toy Biz",
+        price: 19.99
+    },
+    {
+        id: 2,
+        name: "Hulk Hands",
+        brand: "Marvel",
+        franchise: "The Incredible Hulk",
+        age: "3+",
+        manufacturer: "Toy Biz",
+        price: 19.99
+    }
+]
+
+console.log(toys)
